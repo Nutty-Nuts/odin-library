68eeefb4584adf8f84bbe451c3c97fe302c60240
Nutty-Nuts <vegagerard1130@gmail.com>
2022-08-17 10:57:16 +0800

    added todos

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// TODO: Refactor Code
+// TODO: Implement Data Attribute Values in HTML for ease of Access
+// TODO: Get the Read Status Button Working per Instance
+// TODO: Get the Remove Button working per Instance
+
 let libraryCollection = [];
 
 menu = document.getElementById("menu-modal");
